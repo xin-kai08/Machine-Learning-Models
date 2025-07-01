e8e2f04aa598d29a481ebb0d861ca5b3255694bd
xin-kai08 <boss921008@gmail.com>
2025-07-01 16:07:05 +0800

    Update esp_to_python.ino

--- esp to python/arduino/esp_to_python.ino
+++ esp to python/arduino/esp_to_python.ino
@@ -1,12 +1,16 @@
 #include <Wire.h>
 #include <Adafruit_INA219.h>
+#include <Adafruit_MLX90614.h>
 
 Adafruit_INA219 ina219;
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 void setup() {
   // === 初始化 Serial 與 INA219 ===
   Serial.begin(9600);
+  Wire.begin();
   ina219.begin();
+  mlx.begin();
 }
 
 void loop() {
@@ -14,9 +18,7 @@ void loop() {
   float busVoltage = ina219.getBusVoltage_V();  // 系統電壓 (V)
   float current_mA = ina219.getCurrent_mA();    // 電流 (mA)
   float power_mW   = ina219.getPower_mW();      // 功率 (mW)
-
-  // === 假設你還有溫度，可自行換成正確感測值 ===
-  float temp = 25.0;
+  float temp = mlx.readObjectTempC();           // 物體溫度 (°C)
 
   // === 串口傳送: 電流(A), 電壓(V), 功率(W), 溫度(C) ===
   Serial.print(current_mA / 1000.0, 6);  // 換成 A，保留 6 位小數
